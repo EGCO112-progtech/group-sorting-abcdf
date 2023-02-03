d821a1a13af7c44dc418e322ab75902a751e73f3
UncleGun <kunanont.vut@student.mahidol.edu>
2023-02-03 18:31:13 +0700

    test

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
-#define N 6
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include "sorting.h"
 
-int main() {
-  int a[N]={3,2,6,7,3,1};
-  int i,j,new_number;
+int main(int argc, char **argv){ //argc=argument count, argv=argument variable
+  int i,j=0,N=argc-2,*a=(int*)malloc(sizeof(int)*N);
+  for(i=0;i<N;i++)
+  {
+    a[i]=atoi(argv[i+2]);
+  }
+//  printf("%s",argv[1]);
   display(a,N);
-  bubbleSort(a,N); 
-   //insertion(a,N);
-
-  // selectionSort(a,N);
+  if (strcmp(argv[1],"bubble")==0) bubbleSort(a,N);
+  else if (strcmp(argv[1],"selection")==0) selectionSort(a,N);
+  else if (strcmp(argv[1],"insertion")==0) insertion(a,N);
   display(a,N);
- return 0;
+  return 0;
 }
-
-
